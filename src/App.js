5073106c1140af83f041eb25dcda3c8af3608378
Nemo <nemo.spaske@live.com>
2021-03-21 00:11:56 +0100

    Contact form done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Services from './components/Services';
 import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
+import Contacts from './components/Contacts';
+
 
 function App() {
   return (
@@ -40,6 +42,7 @@ function App() {
     <Experience />
     <Portfolio />
     <Testimonials />
+    <Contacts />
     </>
   );
 }
