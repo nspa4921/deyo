3c0e708931bb7821195c914238e840c85486eade
Nemo <nemo.spaske@live.com>
2021-03-21 10:27:01 +0100

    Footer completed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
 import Contacts from './components/Contacts';
+import Footer from './components/Footer';
+
 
 
 function App() {
@@ -43,6 +45,7 @@ function App() {
     <Portfolio />
     <Testimonials />
     <Contacts />
+    <Footer />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,13 @@ import Typed from "react-typed";
 
 const Header = () => {
     return (
-        <div className="header-wrapper">
+        <div id="home" className="header-wrapper">
             <div className="main-info">
-                <h1 style={{color: "#55a92d"}}>DEJAN MLADENOVIC</h1> <h2 style={{ color: "rgba(3, 169, 252)"}}> BASKETBALL COACH!</h2>
+            <h2><span class="outline-text">DEJAN MLADENOVIC</span></h2>
+                 <h2 style={{ color: "rgba(3, 169, 252)"}}> BASKETBALL COACH!</h2>
                 <Typed 
                 className="typed-text"
-                strings={["Individaul Training", "Motor skills", "Techical skills"]}
+                strings={["Individaul Trainings", "Motor skills", "Techical skills"]}
                 typeSpeed={40}
                 backSpeed={60}
                 loop
